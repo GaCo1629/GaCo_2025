4fa59f2afbb0f52056b87e028297d1f3b4708b96
Podcakero <github@podcake.net>
2025-04-10 10:54:48 -0400

    Initial Setup of Advantage Kit and GIT Data Logging.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,25 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import com.ctre.phoenix6.SignalLogger;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.generated.BuildConstants;
 import frc.robot.subsystems.ApproachTarget;
 import frc.robot.subsystems.Globals;
 import frc.robot.subsystems.LEDmode;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   public static final Field2d m_field = new Field2d(); // Tele-Op field
@@ -23,6 +30,25 @@ public class Robot extends TimedRobot {
   private final RobotContainer m_robotContainer;
 
   public Robot() {
+    Logger.recordMetadata("ProjectName", "GaCo 2025"); // Set a metadata value
+
+    if (isReal()) {
+        Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+    } else {
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+        Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
+
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
     m_robotContainer = new RobotContainer();
   }
 
@@ -30,7 +56,6 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     SignalLogger.stop();
     CommandScheduler.getInstance().run(); 
-
   }
 
   @Override
---
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.generated;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "GaCo_2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 349;
+  public static final String GIT_SHA = "444359c619f96005046cf92594ffdc72615a9963";
+  public static final String GIT_DATE = "2025-04-08 14:39:09 EDT";
+  public static final String GIT_BRANCH = "AdvantageKit";
+  public static final String BUILD_DATE = "2025-04-10 10:48:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1744296483914L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
