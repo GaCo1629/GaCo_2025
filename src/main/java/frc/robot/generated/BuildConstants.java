2d72d7169124a88def6850f256fd82749af85c15
Podcakero <github@podcake.net>
2025-04-10 12:08:48 -0400

    Update gitignore

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GaCo_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 350;
-  public static final String GIT_SHA = "4fa59f2afbb0f52056b87e028297d1f3b4708b96";
-  public static final String GIT_DATE = "2025-04-10 10:54:48 EDT";
+  public static final int GIT_REVISION = 351;
+  public static final String GIT_SHA = "62c9da7dee967c4536a0ff20d920e10e94b4b5dd";
+  public static final String GIT_DATE = "2025-04-10 12:06:21 EDT";
   public static final String GIT_BRANCH = "AdvantageKit";
-  public static final String BUILD_DATE = "2025-04-10 12:04:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1744301063933L;
+  public static final String BUILD_DATE = "2025-04-10 12:07:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1744301244799L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
