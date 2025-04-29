217bba40c6497baa198d0db80d1bfc5a2fa15b83
GaCo 1629 <59513563+GaCo1629@users.noreply.github.com>
2025-04-28 20:07:24 -0400

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,7 +63,7 @@ public class Constants {
 
         public static final double kAlgaeIntakeAngleDegrees        = 180; 
         public static final double kAlgaeWindupAngleDegrees        = 150; 
-        public static final double kAlgaeReleaseAngleDegrees       = 125;  // was 125
+        public static final double kAlgaeReleaseAngleDegrees       = 130;  // was 125
         public static final double kAlgaeBackspinDegrees           = 95;
         public static final double kAlgaeReleaseGoalAngleDegrees   = 22; // was 20
         
