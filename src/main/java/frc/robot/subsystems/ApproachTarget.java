65879d3f76e3b0db91ef67b7762c056af49ac808
GaCo 1629 <59513563+GaCo1629@users.noreply.github.com>
2025-09-24 17:57:39 -0400

    Tweaked Autos,  Moved Algae approach closer. Added Auto Algae collect on button press.

--- src/main/java/frc/robot/subsystems/ApproachTarget.java
+++ src/main/java/frc/robot/subsystems/ApproachTarget.java
@@ -31,7 +31,7 @@ public enum ApproachTarget {
     REEF_K(19, 6, ApproachPosition.LEFT, false),
     REEF_L(19, 6, ApproachPosition.RIGHT, false),
     REEF_KL(19, 6, ApproachPosition.ALGAE, false),
-    PROCESSOR(16, 3, ApproachPosition.ALGAE, false),
+    PROCESSOR(16, 3, ApproachPosition.PROCESSOR, false),
     BARGE(14, 5, ApproachPosition.OVERHEAD, true),
     LEFT_SOURCE(13, 1, ApproachPosition.ALGAE, true),
     RIGHT_SOURCE(12, 2, ApproachPosition.ALGAE, true);
@@ -54,7 +54,7 @@ public enum ApproachTarget {
 
     private ApproachTarget(int blueTagId, int redTagId, ApproachPosition position, boolean enableHighCam){
 
-        System.err.println("AprilTags Initialized");
+        // System.err.println("AprilTags Initialized");
 
         this.blueTagId = blueTagId;
         this.redTagId = redTagId;
